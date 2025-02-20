5457d8e17d41c0779148b8ab0189e51c1fbc0544
consoleLogRitik <ritikchoubey12102@gmail.com>
2025-02-20 15:20:04 +0530

    database fixes

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -16,7 +16,7 @@ if(!cached) {
   }
 }
 
-export const connectToDb = async () => {
+export const connectTODb = async () => {
   if(cached.conn) return cached.conn;
 
   if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
