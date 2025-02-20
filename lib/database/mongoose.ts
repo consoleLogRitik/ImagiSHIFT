5aea4754c03093f995d15d4bbb19fa9cc5bbafd1
consoleLogRitik <ritikchoubey12102@gmail.com>
2025-02-20 15:17:00 +0530

    database fixes

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,32 +1,33 @@
 /*eslint-disable @typescript-eslint/no-explicit-any */
-import mongoose ,{Mongoose} from 'mongoose';
+import mongoose, { Mongoose } from 'mongoose';
 
-//get mongodb url from env file
-const MONGODB_URl  = process.env.MONGODB_URL;
+const MONGODB_URL = process.env.MONGODB_URL;
 
-//make interface 
-interface MongooseConnection{
-    conn:Mongoose | null;
-    promise: Promise<Mongoose> | null
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
-//make a cache 
-let cached : MongooseConnection = (global as any).mongoose
+let cached: MongooseConnection = (global as any).mongoose
 
-if(!cached){
-    cached = (global as any).mongoose = {
-        conn:null,promise:null
-    }
+if(!cached) {
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
 }
 
-//connection function writing
-export const connectionTODb = async () => {
-    if (cached.conn) return cached.conn
-    if(!MONGODB_URl) throw new Error('missing mongodb url')
+export const connectToDatabase = async () => {
+  if(cached.conn) return cached.conn;
 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URl,{
-        dbName:'ImagiShift',bufferCommands:false
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'imaginify', bufferCommands: false 
     })
-    cached.conn = await cached.promise
-    return cached.conn;
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
 }
